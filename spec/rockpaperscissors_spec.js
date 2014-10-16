be971fb69ebc7b95a5e554330fc48133dacf59d3
Fadie Hannona <fadiehannona@gmail.com>
2014-10-16 11:40:03 +0100

    Added rules testing

--- spec/rockpaperscissors_spec.js
+++ spec/rockpaperscissors_spec.js
@@ -1,23 +1,31 @@
 describe('Rock Paper Scissors', function () {
 
+// // // // // // // GESTURES TEST // // // // // // //
+
 	describe('Gestures', function () {
 
 		it ('Rock', function () {
 			rock = new Rock
 			expect(rock.type).toEqual('Rock')
-		})
+		});
 
 		it ('Scissors', function (){
 			scissors = new Scissors
 			expect(scissors.type).toEqual('Scissors')
-		})
+		});
 
 		it ('Paper', function () {
 			paper = new Paper
 			expect(paper.type).toEqual('Paper')
-		})
+		});
 
-	})
+	});
+
+// // // // // // //  RULES TEST // // // // // // //
+
+	describe('Rules', function () {
+	
+	});
 
 
-})
+});
